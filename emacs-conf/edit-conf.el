8b4858e8fc5d8f7ce06fda7d69afaabd68916a17
Jean Louis ARNAULT resp P BLANCHARD (email=jean-louis.arnault@technicolor.com) <arnaultj@renndlxrdl3169.localdomain>
2012-06-29 14:45:36 +0200

    Uniquify lines in an emacs buffer.

--- emacs-conf/edit-conf.el
+++ emacs-conf/edit-conf.el
@@ -89,6 +89,26 @@
   (goto-line line)
   )
 
+(defun uniq-lines (beg end)
+  "Unique lines in region.
+Called from a program, there are two arguments:
+BEG and END (region to sort)."
+  (interactive "r")
+  (save-excursion
+    (save-restriction
+      (narrow-to-region beg end)
+      (goto-char (point-min))
+      (while (not (eobp))
+        (kill-line 1)
+        (yank)
+        (let ((next-line (point)))
+          (while
+              (re-search-forward
+               (format "^%s" (regexp-quote (car kill-ring))) nil t)
+            (replace-match "" nil nil))
+          (goto-char next-line))))))
+
+
 ;; fast goto-line
 (global-set-key (kbd "s-g") 'goto-line)
 
